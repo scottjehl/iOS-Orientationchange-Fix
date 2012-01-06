a9b96894e8924a9623e9b6d9b2fb29ca57a0b329
scottjehl <scott@scottjehl.com>
2012-01-06 10:29:39 +0700

    First.

---
+++ README.md
@@ -0,0 +1,8 @@
+A fix for the iOS orientationchange zoom bug.
+=======================
+
+Authored by @scottjehl, rebounded by @wilto.
+MIT License.
+
+Instructions: include the script, enable your zooms. (I'll fill that out more later...).
+
---
+++ ios-orientationchange-fix.js
@@ -0,0 +1,49 @@
+/*! A fix for the iOS orientationchange zoom bug.
+ Script by @scottjehl, rebound by @wilto.
+ MIT License.
+*/
+(function(w){
+    var doc = w.document;
+
+    if( !doc.querySelectorAll ){ return; }
+
+    var meta = doc.querySelectorAll( "meta[name=viewport]" )[ 0 ],
+        initialContent = meta && meta.getAttribute( "content" ),
+        disabledZoom = initialContent + ", maximum-scale=1.0",
+        enabledZoom = initialContent + ", maximum-scale=10.0",
+        enabled = true,
+        orientation = w.orientation,
+        rotation = 0;
+
+    if( !meta ){ return; }
+
+    function restoreZoom(){
+        meta.setAttribute( "content", enabledZoom );
+        enabled = true;
+    }
+
+    function disableZoom(){
+        meta.setAttribute( "content", disabledZoom );
+        enabled = false;
+    }
+
+    function checkTilt( e ){
+        orientation = Math.abs( w.orientation );
+        rotation = Math.abs( e.gamma );
+
+        if( rotation > 8 && orientation === 0 ){
+            if( enabled ){
+                disableZoom();
+            }   
+        }
+        else {
+            if( !enabled ){
+                restoreZoom();
+            }
+        }
+    }
+
+    w.addEventListener( "orientationchange", restoreZoom, false );
+    w.addEventListener( "deviceorientation", checkTilt, false );
+
+})( this );
