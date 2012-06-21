91d95cee7904d0c66f094e1e4d7c3ce724d43f53
Phil Wareham <phil@designhive.com>
2012-06-21 17:34:12 +0200

    Remove a few spaces that were used instead of tabs. Saves a couple of bytes and stops the annoying nag warning in CodeKit.

--- ios-orientationchange-fix.js
+++ ios-orientationchange-fix.js
@@ -10,45 +10,45 @@
 		return;
 	}
 
-    var doc = w.document;
+	var doc = w.document;
 
-    if( !doc.querySelector ){ return; }
+	if( !doc.querySelector ){ return; }
 
-    var meta = doc.querySelector( "meta[name=viewport]" ),
-        initialContent = meta && meta.getAttribute( "content" ),
-        disabledZoom = initialContent + ",maximum-scale=1",
-        enabledZoom = initialContent + ",maximum-scale=10",
-        enabled = true,
+	var meta = doc.querySelector( "meta[name=viewport]" ),
+		initialContent = meta && meta.getAttribute( "content" ),
+		disabledZoom = initialContent + ",maximum-scale=1",
+		enabledZoom = initialContent + ",maximum-scale=10",
+		enabled = true,
 		x, y, z, aig;
 
-    if( !meta ){ return; }
+	if( !meta ){ return; }
 
-    function restoreZoom(){
-        meta.setAttribute( "content", enabledZoom );
-        enabled = true;
-    }
+	function restoreZoom(){
+		meta.setAttribute( "content", enabledZoom );
+		enabled = true;
+	}
 
-    function disableZoom(){
-        meta.setAttribute( "content", disabledZoom );
-        enabled = false;
-    }
+	function disableZoom(){
+		meta.setAttribute( "content", disabledZoom );
+		enabled = false;
+	}
 	
-    function checkTilt( e ){
+	function checkTilt( e ){
 		aig = e.accelerationIncludingGravity;
 		x = Math.abs( aig.x );
 		y = Math.abs( aig.y );
 		z = Math.abs( aig.z );
 				
 		// If portrait orientation and in one of the danger zones
-        if( (!w.orientation || w.orientation === 180) && ( x > 7 || ( ( z > 6 && y < 8 || z < 8 && y > 6 ) && x > 5 ) ) ){
+		if( (!w.orientation || w.orientation === 180) && ( x > 7 || ( ( z > 6 && y < 8 || z < 8 && y > 6 ) && x > 5 ) ) ){
 			if( enabled ){
 				disableZoom();
-			}        	
-        }
+			}
+		}
 		else if( !enabled ){
 			restoreZoom();
-        }
-    }
+		}
+	}
 	
 	w.addEventListener( "orientationchange", restoreZoom, false );
 	w.addEventListener( "devicemotion", checkTilt, false );
