9e59faea3386ff307593553cfc87f3af0c7453f0
Peter Wooster <peter@wooster.ca>
2012-09-05 11:08:15 -0400

    fix broken version

--- ios-orientationchange-fix.js
+++ ios-orientationchange-fix.js
@@ -1,28 +1,30 @@
-/*! A fix for the iOS Safari orientationchange zoom bug.
- Script by @scottjehl, rebound by @wilto, modified by @peterwooster.
+/*! A fix for the iOS orientationchange zoom bug.
+ Script by @scottjehl, rebound by @wilto, modified by Peter Wooster.
  MIT / GPLv2 License.
 */
+
 (function(w){
 	
-	// This fix addresses an iOS Safari bug, so return early if the UA claims it's something else.
+	// This fix addresses an Mobile Safari iOS bug, so return early if the UA claims it's something else.
 	var ua = navigator.userAgent;
-	if( !( /iPhone|iPad|iPod/.test( navigator.platform ) && /OS [1-5]_[0-9_]* like Mac OS X/i.test(ua) && ua.indexOf( "AppleWebKit" ) > -1  && ua.indexOf( "CriOS") == -1 ) ){
+	if( !( /iPhone|iPad|iPod/.test( navigator.platform ) 
+            && /OS [1-5]_[0-9_]* like Mac OS X/i.test(ua) 
+            && ua.indexOf( "AppleWebKit" ) > -1
+            && ua.indexOf( "CriOS") == -1  // chrome for iOS doesn't have the bug
+            )){
 		return;
 	}
 
     var doc = w.document;
 
     if( !doc.querySelector ){ return; }
-
-    var meta = doc.querySelector( "meta[name=viewport]" ),
-        initialContent = meta && meta.getAttribute( "content" ),
-        disabledZoom = initialContent + ",maximum-scale=1",
-        enabledZoom = initialContent + ",maximum-scale=10",
-        enabled = true,
-		x, y, z, aig;
-
+    var meta = doc.querySelector( "meta[name=viewport]" );
     if( !meta ){ return; }
-
+    var initialContent = meta && meta.getAttribute( "content" );
+    var disabledZoom = initialContent + ",maximum-scale=1";
+    var enabledZoom = initialContent + ",maximum-scale=10";
+    var enabled = true;
+    var	x, y, z, aig;
     function restoreZoom(){
         meta.setAttribute( "content", enabledZoom );
         enabled = true;
@@ -34,22 +36,24 @@
     }
 	
     function checkTilt( e ){
-		aig = e.accelerationIncludingGravity;
-		x = Math.abs( aig.x );
-		y = Math.abs( aig.y );
-		
-        var s = x/y;
+        var ori = w.orientation;
+        // if it's landscape we're out of here
+        if(90 == Math.abs(w.orientation)) {
+            if(enabled)restoreZoom();
+            return;
+        } 
+        
+	aig = e.accelerationIncludingGravity;
+	x = Math.abs( aig.x );
+	y = Math.abs( aig.y );
+	
         // If in the danger zone where x is much greater than y turn off zoom
-        if(s > 1.2){
-            if(enabled){
-            	disableZoom();
-            }
-        }else if( !enabled ){
-        	restoreZoom();
-        }
+        if(y == 0 || (x/y) > 1.2){
+            if(enabled)disableZoom();
+        }else if( !enabled )restoreZoom();
     }
 	
-	w.addEventListener( "orientationchange", restoreZoom, false );
-	w.addEventListener( "devicemotion", checkTilt, false );
+    w.addEventListener( "orientationchange", restoreZoom, false );
+    w.addEventListener( "devicemotion", checkTilt, false );
 
-})( this );
+})( this );    
